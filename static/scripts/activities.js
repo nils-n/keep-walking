3b10c1af36c2a60d653663a0540db19db36cee81
Nils <52067471+nils-n@users.noreply.github.com>
2023-09-01 14:22:10 +0100

    add data-id to button to identify which entry to perform CRUD when clicked

--- static/scripts/activities.js
+++ static/scripts/activities.js
@@ -1,5 +1,20 @@
 
-$('.edit-button').click( () => {
+$('.edit-button').click( (e) => {
     console.log('Edit button clicked!  ')
+    const button = $(e.target)
+    const garmin_id = button.attr('data-id')
+    console.log(garmin_id)
+})
+
+$('.delete-button').click( (e) => {
+    console.log('Delete button clicked!  ')
+    const button = $(e.target)
+    const garmin_id = button.attr('data-id')
+    console.log(garmin_id)
+    // console.log(.attr('data-id'))
+    // const garmin_data_id = $(this).siblings('.garmin-id')
+    // const garmin_data_id_for_delete = garmin_data_id[0]
+    // console.log(garmin_data_id)
+    // console.log(garmin_data_id_for_delete)
 
 })
