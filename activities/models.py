15b4b19f652f466c4172f1f8f50d8514b3666619
Nils <52067471+nils-n@users.noreply.github.com>
2023-09-02 19:17:47 +0100

    add emotion model

--- activities/models.py
+++ activities/models.py
@@ -19,3 +19,25 @@ class GarminData(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Emotion(models.Model):
+    class EmotionRating(models.TextChoices):
+        BAD = "0", "BAD"
+        NEUTRAL = "1", "NEUTRAL"
+        GOOD = "2", "GOOD"
+        UNDEFINED = "3", "UNDEFINED"
+
+    activity = models.ForeignKey(
+        GarminData, on_delete=models.CASCADE, related_name="emotion_rating"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="emotion_rating",
+    )
+    rating = models.IntegerField(
+        choices=EmotionRating.choices, default=EmotionRating.UNDEFINED
+    )
+    created_at = models.DateField(auto_now=True)
+    updated_at = models.DateField(auto_now=True)
