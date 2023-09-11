40c2483779c3b0dbcb970f8f3a675e14e0bbb9bd
Nils <52067471+nils-n@users.noreply.github.com>
2023-09-11 11:24:12 +0100

    deployment commit for heroku

--- keep_walking/settings.py
+++ keep_walking/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
