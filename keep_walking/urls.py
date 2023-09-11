3054b2a496fe9a00316902caae4552fb84b5c0f8
Nils <52067471+nils-n@users.noreply.github.com>
2023-09-11 11:56:42 +0100

    deployment commit for heroku

--- keep_walking/urls.py
+++ keep_walking/urls.py
@@ -25,9 +25,5 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("activities/", include("activities.urls")),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
-
-
-urlpatterns = [
-    # ... the rest of your URLconf goes here ...
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
