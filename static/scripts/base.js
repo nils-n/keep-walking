f8ba46cd5fba3e496d9bb5a49f7fc5c11863871d
Nils <52067471+nils-n@users.noreply.github.com>
2023-09-11 10:52:52 +0100

    make navbar responsive for all screen sizes

---
+++ static/scripts/base.js
@@ -0,0 +1,19 @@
+console.log("well well well");
+
+//toggle hamburger button when clicked on navbar
+const navButton = document.getElementById("hamburger-button");
+navButton.addEventListener("click", (e) => {
+  hamburgerIcon = document.getElementById("hamburger-nav");
+  crossIcon = document.getElementById("cross-nav");
+  navLinks = document.getElementById("nav-links");
+
+  if (hamburgerIcon.classList.contains("hidden")) {
+    hamburgerIcon.classList.remove("hidden");
+    crossIcon.classList.add("hidden");
+    navLinks.classList.add("hidden");
+  } else {
+    hamburgerIcon.classList.add("hidden");
+    crossIcon.classList.remove("hidden");
+    navLinks.classList.remove("hidden");
+  }
+});
