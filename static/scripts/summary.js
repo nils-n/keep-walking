94e4d2a910e25a1849a50ab85f391df236516d9e
Nils <52067471+nils-n@users.noreply.github.com>
2023-09-20 19:21:46 +0100

    ensure that javascript function also runs in the browser

--- static/scripts/summary.js
+++ static/scripts/summary.js
@@ -1,3 +1,6 @@
 console.log("entering summary.js now...");
 
 console.log("the answer is probably ...... " + sum(32, 10));
+console.log("   A BMI of   16 is -->  " + getBMIstatus(16));
+console.log("   A BMI of   20 is -->  " + getBMIstatus(20));
+console.log("   A BMI of   28  is -->  " + getBMIstatus(28));
