017fcd6dc253b63876edeeb695bd17dfc58686e7
Nils <52067471+nils-n@users.noreply.github.com>
2023-10-07 15:24:16 +0100

    refactor tests of profile view into a single parametrized test

--- activities/test_views.py
+++ activities/test_views.py
@@ -413,35 +413,49 @@ def test_unauthenticated_user_can_access_home_page(client, django_user_model):
     assert response.status_code == 200
 
 
-def test_authenticated_user_can_edit_own_profile_page(
-    client, django_user_model
+@pytest.mark.parametrize()(
+    "client_fixture_name, model_fixture_name, url, expected_status_code",
+    [
+        (
+            "client",
+            "django_user_model",
+            "/activities/profile/1/edit_profile",
+            200,
+        ),
+        (
+            "client",
+            "django_user_model",
+            "/activities/profile/2/edit_profile",
+            403,
+        ),
+        (
+            "client",
+            "django_user_model",
+            "/activities/profile/1/delete_profile",
+            200,
+        ),
+        (
+            "client",
+            "django_user_model",
+            "/activities/profile/2/delete_profile",
+            403,
+        ),
+    ],
+)
+def test_only_authenticated_users_can_edit_and_delete_own_profile(
+    client_fixture_name, model_fixture_name, url, expected_status_code, request
 ):
     """
     this tests whether a signed in user can edit their profile page
     """
+    client = request.getfixturevalue(client_fixture_name)
+    django_user_model = request.getfixturevalue(model_fixture_name)
+
     username = "testuser2"
     password = "1234-abcd"
     user = django_user_model.objects.create_user(
         username=username, password=password
     )
     client.login(username=username, password=password)
-    response = client.get("/activities/profile/1/edit_profile")
-    assert response.status_code == 200
-
-
-def test_authenticated_user_cannot_edit_profile_page_of_someone_else(
-    client, django_user_model
-):
-    """
-    this tests whether a signed in user is denied to edit other users profile page
-    """
-    usernames = ["testuser1", "testuser2"]
-    passwords = ["1234-abcd", "abcd-1234"]
-    for username, password in zip(usernames, passwords):
-        user = django_user_model.objects.create_user(
-            username=username, password=password
-        )
-
-    client.login(username=usernames[0], password=passwords[0])
-    response = client.get("/activities/profile/2/edit_profile")
-    assert response.status_code == 403
+    response = client.get(url)
+    assert response.status_code == expected_status_code
